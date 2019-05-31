3f9b4c4f9cb75c81275e61583d6f494803738984
richhorvath <richhorvath@users.noreply.github.com>
2019-05-31 19:30:53 -0400

    Completed card class with constructors setter/getter methods

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,24 @@
 public class Card {
-    
+    private String question;
+    private String answer;
+
+    public Card(String question, String answer){
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public void setQuestion(String question){
+        this.question = question;
+    }
+    public String getQuestion(){
+        return question;
+    }
+    public void setAnswer(String answer){
+        this.answer = answer;
+    }
+    public String getAnswer(){
+        return answer;
+    }
+
+
 }
