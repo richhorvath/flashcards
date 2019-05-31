bea9ce109d69689f8d9a08a13106304718e3f94b
richhorvath <richhorvath@users.noreply.github.com>
2019-05-31 19:26:08 -0400

    Added card creation UI and started card class

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,71 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class GUI {
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList cardList;
+    private JFrame frame;
     public void init(){
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        // build gui
+        frame = new JFrame("Quiz Card Builder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // title bar
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+        question = new JTextArea(6,20);
+        question.setLineWrap(true);
+        question.setWrapStyleWord(true);
+        question.setFont(bigFont);
+
+        JScrollPane qScroller = new JScrollPane(question);
+        qScroller.setVerticalScrollBarPolicy(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        answer = new JTextArea(6,20);
+        answer.setLineWrap(true);
+        answer.setWrapStyleWord(true);
+        answer.setFont(bigFont);
+
+        JScrollPane aScroller = new JScrollPane(answer);
+        aScroller.setVerticalScrollBarPolicy(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        aScroller.setHorizontalScrollBarPolicy(
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        JButton nextButton = new JButton("Next Card");
+        cardList = new ArrayList();
+        JLabel qLabel = new JLabel("Question:");
+        JLabel aLabel = new JLabel("Answer:");
+
+        mainPanel.add(qLabel);
+        mainPanel.add(qScroller);
+        mainPanel.add(aLabel);
+        mainPanel.add(aScroller);
+        mainPanel.add(nextButton);
+        //create menubar add jmenu file and item new
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New Deck");
+
+
+
+        fileMenu.add(newMenuItem);
+
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500,600);
         frame.setVisible(true);
     }
+
+    public void cardBulder(){
+
+    }
+
+    //card builder
+    //card player
 }
